e28d76f20637b87e4ca56fbffe21d2253c8862dd
inoricode28 <inori237@gmail.com>
2023-05-06 02:53:27 -0500

    MySql02

--- Ejemplos.sql
+++ Ejemplos.sql
@@ -5,11 +5,13 @@ CREATE DATABASE<db>;
 -- Crear tabla
 
 CREATE TABLE users (
-    id
-    username
-    email
-    age
-    status
-    bio    
-    created_at
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    age INT UNSIGNED NOT NULL,
+    status ENUM('active','inactive') DEFAULT 'inactive',
+    bio TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users(username, email, age) VALUES ('user2','user2@test.com',18);
